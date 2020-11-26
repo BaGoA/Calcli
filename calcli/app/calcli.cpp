463aeb2637135eb730345a2c83a6cd694f715ddd
Bastian Gonzalez Acevedo <bastiangonzalezacevedo@gmail.com>
2020-11-26 11:34:25 +0100

    Update Calcli application with postfix evaluation

--- calcli/app/calcli.cpp
+++ calcli/app/calcli.cpp
@@ -21,8 +21,6 @@
 
 #include <calcli/app/calcli.hpp>
 
-#include <iostream>
-
 #include <calcli/core/lexer.hpp>
 #include <calcli/core/evaluator.hpp>
 
@@ -31,24 +29,9 @@ double calcli::compute(const std::string_view& expression)
 {
 	const std::vector<calcli::token> tokens = calcli::tokenize(expression);
 
-	std::cout << "Lexer output:\n";
-	
-	for(const auto& token : tokens)
-	{
-		std::cout << "Type = " << token.type << " Value = " << token.value.c_str() << "\n";
-	}
-
-	std::cout << "\n";
-	std::cout << "Convertion to postfix output:\n";
-
 	const std::vector<calcli::token> tokens_postfix = calcli::infix_to_postfix(tokens);
 
-	for(const auto& token : tokens_postfix)
-	{
-		std::cout << "Type = " << token.type << " Value = " << token.value.c_str() << "\n";
-	}
+	const double value = calcli::postfix_evaluation(tokens_postfix);
 
-	std::cout << "\n";
-
-	return 0.0;
+	return value;
 }
